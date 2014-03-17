306379fcf0f344c05718ccf53f891aecfc1ab59a
matt <matt@madeye.com>
2014-03-17 10:47:17 +0000

    Updated version following release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.madeye.clojure.notifymyandroid/notifymyandroid "0.1.5"
+(defproject com.madeye.clojure.notifymyandroid/notifymyandroid "0.1.6"
   :description "Clojure bindings for Notify My Android"
   :url "https://github.com/madeye-matt/notifymyandroid"
   :license {:name "Eclipse Public License"
